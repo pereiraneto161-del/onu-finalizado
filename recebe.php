278cc813582a7dac4480ea69beee0a237c55a6b3
netinho_pereira <pereiraneto161@gmail.com>
2025-10-29 19:17:46 -0300

    Add recebe.php for handling form submissions

---
+++ recebe.php
@@ -0,0 +1,25 @@
+<?php
+    $comentario = $_POST['comentario'];
+    $pais = $_POST['pais'];
+
+    $host = 'localhost';
+    $db = 'onu';
+    $user = 'root';
+    $password = '';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    try{
+        $pdo = new PDO(dsn: $dsn, username: $user, password: $password);
+        header("Location: sucesso.html");
+    }catch(\PDOExecption $e){
+        throw new \PDOExecption(message: $e->getMessage()). code;
+        (int)$e->getcode();
+    }
+
+    $sql = "INSERT INTO onu (comentario, pais) VALUES(?,?)";
+
+    $stmt = $pdo->prepare(query: $sql);
+
+    $stmt->execute(params: [$comentario, $pais]);
+?>
